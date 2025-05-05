efbfbfc933dea09cbdc4878e20cc8a2ba4460322
J <jgarcia3327@gmail.com>
2025-05-05 06:53:54 -0400

    Add image view WIP

--- resources/js/Pages/Shopping/Partials/ShoppingImageDisplay.tsx
+++ resources/js/Pages/Shopping/Partials/ShoppingImageDisplay.tsx
@@ -1,5 +1,5 @@
 import { ShoppingImageProps } from "@/types/shopping";
-
+import { Carousel } from "@material-tailwind/react"
 
 export default function ShoppingImageDisplay({shoppingImages}:{
     shoppingImages?: [ShoppingImageProps]
@@ -15,12 +15,30 @@ export default function ShoppingImageDisplay({shoppingImages}:{
     }
 
     return (
-        <>
-            {shoppingImages && shoppingImages.map((i:ShoppingImageProps) => 
-                <div key={i.id}>
-                    <img src={'/storage/assets/images/shopping/' + i.name} />
+        <Carousel 
+            className="rounded-xl"
+            navigation={({ setActiveIndex, activeIndex, length }) => (
+                <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+                  {new Array(length).fill("").map((_, i) => (
+                    <span
+                      key={i}
+                      className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                        activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+                      }`}
+                      onClick={() => setActiveIndex(i)}
+                    />
+                  ))}
                 </div>
+              )}
+        >
+            {shoppingImages && shoppingImages.map((i:ShoppingImageProps, index) => 
+                <img
+                    key={i.id}
+                    src={'/storage/assets/images/shopping/' + i.name}
+                    alt={"image " + index}
+                    className="max-h-96 max-w-3xl object-contain"
+                />
             )}
-        </>
+        </Carousel>
     )
 }
